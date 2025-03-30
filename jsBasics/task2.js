027c0af4373db8da20f0559e5f0a22de749069c6
neliapravdenko <pravdenko2001@gmail.com>
2025-03-30 22:04:07 +0300

    Modify task2

---
+++ jsBasics/task2.js
@@ -0,0 +1,8 @@
+const firstName = "Marc";
+const secondName = "Alice";
+const greetings = "Hello " + firstName + " and " + secondName + ", it's nice to see you here!";
+const greetings2 = `Hello ${firstName} and ${secondName}, it's nice to see you here!`;
+
+
+console.log(greetings);
+console.log(greetings2);
