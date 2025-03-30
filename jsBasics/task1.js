bb8084ee61ff21e09f1775174b9b7feb467dbfe7
neliapravdenko <pravdenko2001@gmail.com>
2025-03-30 21:52:46 +0300

    Modify task1

---
+++ jsBasics/task1.js
@@ -0,0 +1,11 @@
+const textLine = "Homework";
+const homeworkNumber = 4;
+const isManualQa = true;
+const emptyItem = null;
+let undefinedItem;
+
+console.log(textLine);
+console.log(homeworkNumber);
+console.log(isManualQa);
+console.log(emptyItem);
+console.log(undefinedItem);
