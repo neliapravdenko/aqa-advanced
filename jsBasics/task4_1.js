c662e98f15ba7c451266d7972966c45962ed643d
neliapravdenko <pravdenko2001@gmail.com>
2025-03-30 22:45:51 +0300

    Modify task4_1

---
+++ jsBasics/task4_1.js
@@ -0,0 +1,5 @@
+const radius = +(Math.random() * 10 + 1).toFixed(2);
+console.log(`Radius is: ${radius}`);
+
+const circleArea = +(Math.PI * Math.pow(radius, 2)).toFixed(2);
+console.log(`Circle area = ${circleArea}`);
