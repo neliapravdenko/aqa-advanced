4db3f4cd4100bb6e9e937a286f87e52993cff8c1
neliapravdenko <pravdenko2001@gmail.com>
2025-03-30 23:21:39 +0300

    Change task4_1 and task4_2

--- jsBasics/task4_1.js
+++ jsBasics/task4_1.js
@@ -1,5 +1,5 @@
-const radius = +(Math.random() * 10 + 1).toFixed(2);
-console.log(`Radius is: ${radius}`);
+const radius = Math.random() * 10 + 1;
+console.log(`Radius is: ${radius.toFixed(2)}`);
 
 const circleArea = +(Math.PI * Math.pow(radius, 2)).toFixed(2);
 console.log(`Circle area = ${circleArea}`);
--- jsBasics/task4_2.js
+++ jsBasics/task4_2.js
@@ -1,7 +1,7 @@
-const length = Math.round(Math.random() * 50 + 1);
-const width = Math.round(Math.random() * 25 + 1);
-const rectangleArea = (length * width);
+const length = Math.random() * 50 + 1;
+const width = Math.random() * 25 + 1;
+const rectangleArea = length * width;
 
 console.log(`Length = ${length}`);
 console.log(`Width = ${width}`);
-console.log(`The area of rectangle is ${rectangleArea}`);
+console.log(`The area of rectangle is ${rectangleArea.toFixed(2)}`);
