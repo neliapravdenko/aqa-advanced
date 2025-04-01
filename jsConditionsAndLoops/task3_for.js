ab8ed5118699ee343187dc3412022058585ee784
neliapravdenko <pravdenko2001@gmail.com>
2025-04-01 18:44:01 +0300

    Group and modify task3

--- jsConditionsAndLoops/task3_for.js
+++
@@ -1,8 +0,0 @@
-const number = Math.round(Math.random() * 10);
-console.log(`Число: ${number}`);
-
-
-for (let i = 1; i <= 10; i++) {
-    const result = number * i;
-    console.log(`${number} x ${i} = ${result}`);
-};
