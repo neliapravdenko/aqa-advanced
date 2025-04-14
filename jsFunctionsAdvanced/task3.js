e1c6d4854737d7d5025d6476f962a1f677aaad8b
neliapravdenko <pravdenko2001@gmail.com>
2025-04-14 18:45:19 +0300

    Modify task3.js

---
+++ jsFunctionsAdvanced/task3.js
@@ -0,0 +1,35 @@
+const divide = (numerator, denominator) => {
+
+    if (denominator === 0) {
+        throw new Error("Divided by zero is not allowed!");
+    } else if (typeof numerator !== "number" || typeof denominator !== "number") {
+        throw new Error("Both arguments must be numbers!")
+    }
+    return numerator / denominator;
+}
+
+try {
+    divide(102, 15);
+} catch (error) {
+    console.error("Error occured: ", error.message);
+} finally {
+    console.log("The work is over.");
+}
+
+try {
+    divide(23, 0);
+
+} catch (error) {
+    console.error("Error occured: ", error.message);
+} finally {
+    console.log("The work is over.");
+}
+
+try {
+    divide("hi", 8);
+
+} catch (error) {
+    console.error("Error occured: ", error.message);
+} finally {
+    console.log("The work is over.");
+}
