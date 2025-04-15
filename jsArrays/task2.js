2c3a93a804c77bb070c2fe4724c620ac0e0deedf
neliapravdenko <pravdenko2001@gmail.com>
2025-04-15 16:15:41 +0300

    Change task2.js

--- jsArrays/task2.js
+++ jsArrays/task2.js
@@ -1,5 +1,5 @@
-const initialArray = [1, 2, 3, 4, 5, 2];
+const numbers = [1, 2, 3, 4, 5, 2];
 
-const multipliedArray = initialArray.map((number, index) => number * index);
-console.log(multipliedArray);
+const multipliedNumbers = numbers.map((number, index) => number * index);
+console.log(multipliedNumbers);
 
