74bb97d41fb37375e3c7c03dda50cd7ed3de52c9
neliapravdenko <pravdenko2001@gmail.com>
2025-04-26 22:44:41 +0300

    Add second part of task2

--- jsPromises/task2.js
+++ jsPromises/task2.js
@@ -1,5 +1,5 @@
-function fetchData(id) {
-    return fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
+function getTodoObj(itemId) {
+    return fetch(`https://jsonplaceholder.typicode.com/todos/${itemId}`)
         .then(response => {
             if (!response.ok) {
                 throw new Error(`Error status code: ${response.status}`)
@@ -8,12 +8,30 @@ function fetchData(id) {
         })
 }
 
-fetchData(1)
-    .then(todo => {
-        console.log(todo);
-        return todo;
-    })
-    .catch(error => {
-        console.error(error.message);
-    })
 
+function getUsersObj(userId) {
+    return fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Error status code: ${response.status}`)
+            }
+            return response.json();
+        })
+}
+
+const todoPromise = getTodoObj(1);
+const usersPromise = getUsersObj(1);
+
+const allPromises = Promise.all([todoPromise, usersPromise])
+    .then(([todo, user]) => {
+        console.log("All results: ", { todo, user });
+        return { todo, user };
+    })
+    .catch(error => console.error("Error in Promise.All: ", error.message));
+
+const racePromise = Promise.race([todoPromise, usersPromise])
+    .then(result => {
+        console.log("Race winner: ", result);
+        return result;
+    })
+    .catch(error => console.error("Error in Promise.race: ", error.message));
